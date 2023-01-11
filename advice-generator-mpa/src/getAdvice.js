6e79ba76d92dfbe78c3e2f13d4e105187b7d5085
nadiauj <nadiauj@ext.inditex.com>
2023-01-11 21:00:30 +0100

    Adding advice generator spa and mpa

---
+++ advice-generator-mpa/src/getAdvice.js
@@ -0,0 +1,7 @@
+const getAdvice = async () => {
+    const response = await fetch("https://api.adviceslip.com/advice", {method: 'GET'})
+    const resp = await response.json()
+    return resp.slip.advice
+} 
+
+export default getAdvice
---
+++ advice-generator-spa/src/AdviceGenerator.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import { getAdvice } from "./getAdvice"
+import './index.css'
+
+const AdviceGenerator = ({advice}) => {
+    const [response, setResponse] = useState(advice)
+
+    const onClickButton = async () => {
+        const response = await getAdvice()
+        setResponse(response)
+    }
+
+    return (
+        <div className="advice-container">
+            <label>{response}</label>
+            <button onClick={() => onClickButton()}>Get advice</button>
+        </div>
+    )
+
+}
+
+export default AdviceGenerator
